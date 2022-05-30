81c9b542fe14a8a7f570adb1265deb46ac86a221
Shrey Maurya <shreymaurya2000@gmail.com>
2022-05-31 00:42:48 +0530

    es-lint fixed

--- src/components/CoinsTable.jsx
+++ src/components/CoinsTable.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { CoinList } from "../config/api";
 import { CryptoState } from "../CryptoContext";
 import { useNavigate } from "react-router-dom";
 import {
--- src/components/banner/Carousel.jsx
+++ src/components/banner/Carousel.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
 import { makeStyles } from "@material-ui/core"
 import axios from 'axios';
